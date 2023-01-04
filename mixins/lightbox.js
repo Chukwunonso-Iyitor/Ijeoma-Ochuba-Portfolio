487c94910bbd723d049a8ad850772853eb1ca15a
ROBERT_IYITOR <nonsoiyitor@hotmail.com>
2023-01-04 23:29:22 +0000

    removed unused variables

--- mixins/lightbox.js
+++ mixins/lightbox.js
@@ -1,20 +1,16 @@
-// Lightbox Mixin
+"usestrict";
 
 export const openLightbox = (e) => {
   let src = e.target.getAttribute("src");
-  const lbox = document.getElementById("lightbox");
-  const wrapper = document.querySelector(".img-wrapper");
-  const limg = document.getElementById("lightbox-img");
 
-  lbox.style.cssText = "visibility: visible; opacity: 1";
-  wrapper.style.cssText = "scale: 1;";
-  limg.setAttribute("src", src);
+  document.getElementById("lightbox").style.cssText =
+    "visibility: visible; opacity: 1";
+  document.querySelector(".img-wrapper").style.cssText = "scale: 1;";
+  document.getElementById("lightbox-img").setAttribute("src", src);
 };
 export const closeLightbox = () => {
-  const lbox = document.getElementById("lightbox");
-  const wrapper = document.querySelector(".img-wrapper");
-  const limg = document.getElementById("lightbox-img");
-  lbox.style.cssText = "visibility: hidden; opacity: 0; cursor: auto;";
-  wrapper.style.cssText = "scale: .2;";
-  limg.setAttribute("src", "");
+  document.getElementById("lightbox").style.cssText =
+    "visibility: hidden; opacity: 0; cursor: auto;";
+  document.querySelector(".img-wrapper").style.cssText = "scale: .2;";
+  document.getElementById("lightbox-img").setAttribute("src", "");
 };
