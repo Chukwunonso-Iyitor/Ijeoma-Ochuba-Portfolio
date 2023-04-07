426456412963f9e42d8b84653fa1886cbbf646a6
ROBERT_IYITOR <nonsoiyitor@hotmail.com>
2023-04-07 16:32:16 +0100

    Changed cta on hero banner

--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -90,7 +90,7 @@ const HeroBanner = ({ home }) => {
                 href="/contact"
                 className="btn-orange me-2 px-4 d-inline-block me-5"
               >
-                Hire me <i className="bi bi-hand-thumbs-up-fill ms-1"></i>
+                Contact me <i className="bi bi-hand-thumbs-up-fill ms-1"></i>
               </Link>
               <button type="button" className="btn-arrow" onClick={projects}>
                 See Projects
